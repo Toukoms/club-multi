f5649d7801577b6817cdf97196eb8bf59f161d3d
Toukom's <tokiniaina090605@gmail.com>
2024-05-16 20:54:25 +0300

    fix: lazy loading

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,8 @@
-window.addEventListener("load", () => {
+setTimeout(() => {
   const loading = document.getElementById("loader");
   loading.style.display = "none";
-});
+  document.body.style.overflowY = "scroll";
+}, 2000);
 
 const header = document.getElementsByTagName("header")[0];
 header.classList.toggle("sticky", window.scrollY > 12);
