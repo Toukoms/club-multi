2a956e26e1de72694d8d0933c969b0ac7941dfe4
Toukom's <tokiniaina090605@gmail.com>
2024-05-16 08:49:58 +0300

    redesign website

---
+++ assets/js/teams_data.js
@@ -0,0 +1,202 @@
+const admin_team = [
+  {
+    name: "RAMAHAVATRAHARISOA",
+    firstname: "Ny Anja Manana",
+    image: "./assets/images/teams/NyAnja.jpg",
+    description: `Étant le rapporteur de ce club me permet de transmettre les messages, les consigne et
+        surtout les informations, en favorisant les communications avec  tout le monde.
+        C'est un grand plaisir d'être le messagier de ce club.
+        La créativité n'a pas de limite donc il y a trois réactions possibles à tout design:"Oui, Non et Waouh! La troisième 
+        celle que je vise."`,
+    role: "Rapporteur",
+  },
+  {
+    name: "RAHAJANIRINA",
+    firstname: "Fanomezantsoa Tokiniaina",
+    image: "./assets/images/teams/Toki.jpg",
+    description: `En tant que président du club multimédia, je suis
+        convaincu que chaque membre de notre club possède un
+        potentiel à explorer et un désir intense d'évoluer; une
+        graine qui promet un arbre fruitier. Notre club est bien
+        plus qu'une regroupement d'activités. C'est une source
+        d'inspiration, un laboratoire où les idées prennent forme
+        et où les rêves se réalisent.`,
+    role: "Président",
+  },
+  {
+    name: "RAZAFIARIVELO",
+    firstname: "Miriame Asmine",
+    image: "./assets/images/teams/Asmine.jpg",
+    description: `Être designer est une source de fierté pour moi,car c'est à travers le design que je parviens 
+        à véhiculer des messages,des émotions,tout ce que je peine à exprimer autrement. Je tiens 
+        à souligner que le créateur est le parfait exemple de designer,ses créations m'inspirent et
+        révèlent mes préférences ,mais surtout. Le design ets une réflexion de la vie elle-même.
+        En tant que designer nous contribuons à batir de vie meilleur et du monde magique.`,
+    role: "conseillée",
+  },
+  {
+    name: "LARIO",
+    firstname: "Stanislas",
+    image: "./assets/images/teams/Lario.jpg",
+    description: `Être designer est une source de fierté pour moi,car c'est à travers le design que je parviens 
+        à véhiculer des messages,des émotions,tout ce que je peine à exprimer autrement. Je tiens 
+        à souligner que le créateur est le parfait exemple de designer,ses créations m'inspirent et
+        révèlent mes préférences ,mais surtout. Le design ets une réflexion de la vie elle-même.
+        En tant que designer nous contribuons à batir de vie meilleur et du monde magique.`,
+    role: "Vice Président",
+  },
+  {
+    name: "RANDRIANARISON",
+    firstname: "Kevin",
+    image: "./assets/images/teams/Kevin.jpg",
+    description: `Être designer est une source de fierté pour moi,car c'est à travers le design que je parviens 
+        à véhiculer des messages,des émotions,tout ce que je peine à exprimer autrement. Je tiens 
+        à souligner que le créateur est le parfait exemple de designer,ses créations m'inspirent et
+        révèlent mes préférences ,mais surtout. Le design ets une réflexion de la vie elle-même.
+        En tant que designer nous contribuons à batir de vie meilleur et du monde magique.`,
+    role: "Conseillé",
+  },
+];
+
+const all_teams = [
+  {
+    name: "DJAONASY",
+    firstname: "Pierrot",
+    image: "./assets/images/teams/Pierrot.jpg",
+    description: `La créativité nourrit le design, lui donnant vie et le rendant unique. Ensemble, ils transforment l'imagination en réalité.`,
+    role: "Designer"
+  },
+  {
+    name: "SANDRINO",
+    firstname: "Ickham Jonathan",
+    image: "./assets/images/teams/Jonathan.jpg",
+    description: `En tant que chef du groupe de contenu, on a effectué diverses tâches pour créer le site web vitrine de notre club multimédia en remplisant les éléments visuels et les contenus. Enfin, on a collaboré avec l'équipe pour tester le site.`,
+    role: "Redacteur Web"
+  },
+  {
+    name: "Andriamanohiniaina",
+    firstname: "Herison Jean Freddy",
+    image: "./assets/images/teams/Freddy1.jpeg",
+    description: `Etant que Développeur, je suis un passionné de technologie, un amoureux du code. Chaque jour, je me lève avec l'excitation de découvrir ce que le monde de la programmation me réserve. En tant que développeur web, je suis un explorateur du cyberespace, naviguant à travers des lignes de code pour découvrir de nouvelles terres numériques`,
+    role: "Développeur"
+  },
+  {
+    name: "TSITOHAINA",
+    firstname: "Miharizo Danie",
+    image: "./assets/images/teams/Tsito.jpg",
+    description: `"En tant que rédacteur web du club, je suis poussé par la passion de transformer les idées en mots qui inspirent et éduquent. Chaque article que j'écris est une opportunité d'engager, d'informer et de connecter les membres de notre club, alimentant ainsi ma motivation. Je crois que chaque mot a le pouvoir de faire une différence, et c'est cette conviction qui me motive à donner le meilleur de moi-même à chaque fois que je m'assois pour écrire."`,
+    role: "Redacteur Web"
+  },
+  {
+    name: "KOTONIRINA",
+    firstname: "Nidot",
+    image: "./assets/images/teams/Nidot.jpg",
+    description: `En tant que développeur web, chaque ligne de code est une chance de créer quelque chose de magique sur l'écran de quelqu'un car coder ne se délimite pas au dela d'une balise `,
+    role: "Développeur"
+  },
+  {
+    name: "FENITRINIANA",
+    firstname: "Sylvie",
+    image: "./assets/images/teams/Fenitra.jpg",
+    description: `"En tant que designer web du club, je suis motivé par la possibilité de créer des expériences visuelles qui captivent et engagent. Chaque pixel que je place, chaque couleur que je choisis, est une expression de ma passion pour le design qui améliore la vie des membres de notre club.`,
+    role: "Designer"
+  },
+  {
+    name: "RAZANAKOTO",
+    firstname: "Fenohasina Nantenaina",
+    image: "./assets/images/teams/Nantenaina.jpg",
+    description: `Même dans les plus bas situation qu'on puisse ne voir que le desespoir, il y a toujours un lueur d'espoir.  il faut s'adapter dans le present, le conserver pour pouvoir les resavourer à l'avenir. C'est ce qui me pousse à avancer dans la voie actuelle en aillant nul regret de tous ce que j'ai vécu.`,
+    role: "Développeur"
+  },
+  {
+    name: "RAVELOJAONA",
+    firstname: "Andriatiana Christian",
+    image: "./assets/images/teams/Christian.jpg",
+    description: `La programmation est une forme d'art, où chaque ligne de code est une note de musique dans une symphonie numérique. En tant que développeur web, je suis un compositeur de solutions, un chef d'orchestre du code. Chaque projet est une partition à interpréter, une opportunité de créer quelque chose de beau et de fonctionnel.`,
+    role: "Développeur"
+  },
+  {
+    name: "RAFANOMENZANTSOA",
+    firstname: "Tolotriniaina Valisoa",
+    image: "./assets/images/teams/Valisoa.jpg",
+    description: `La programmation est plus qu'un métier pour moi, c'est une véritable mission. Je crois en la puissance du code pour résoudre des problèmes et transformer des vies. Chaque projet est une opportunité de laisser une empreinte positive dans le cyberespace, une marque de mon dévouement à créer un monde meilleur."`,
+    role: "Développeur"
+  },
+  // {
+  //   name: "RAKOTONJANAHARY ",
+  //   firstname: "Marino",
+  //   image: "./assets/images/teams/Marino.jpg",
+  //   description: `Chaque jour, je me lève avec la passion de créer des expériences numériques qui transcendent les attentes. Mon clavier est ma baguette magique, et mes lignes de code sont des incantations qui donnent vie à des idées. Dans ce monde numérique en constante évolution, je suis l'architecte de l'innovation, façonnant l'avenir pixel par pixel.`,
+  //   role: "Développeur"
+  // },
+  {
+    name: "Andriantsoa ",
+    firstname: "Hajaniaina Rado",
+    image: "./assets/images/teams/Rado.jpg",
+    description: `La programmation est plus qu'un métier pour moi, c'est une véritable mission. Je crois en la puissance du code pour résoudre des problèmes et transformer des vies. Chaque projet est une opportunité de laisser une empreinte positive dans le cyberespace, une marque de mon dévouement à créer un monde meilleur.`,
+    role: "Développeur"
+  },
+  // {
+  //   name: "FAIBATO ",
+  //   firstname: "Clermont Judicaël",
+  //   image: "./assets/images/teams/Clermont.jpg",
+  //   description: `"En tant que créateur de contenu du site web du club, chaque jour est une nouvelle opportunité pour moi de créer des expériences significatives et engageantes. Je suis motivé par le pouvoir des mots et des visuels pour connecter, inspirer et informer. Chaque contenu que je crée est un reflet de ma passion pour l'innovation et l'excellence, et c'est cette passion qui me pousse à continuer à créer, à apprendre et à grandir chaque jour."`,
+  //   role: "Contenu"
+  // },
+  {
+    name: "RA-FANOMEZANA ",
+    firstname: "Herimamy Fenohasina",
+    image: "./assets/images/teams/Fenohasina.jpg",
+    description: `"En tant que créateur de contenu pour notre site web de club, chaque jour est une nouvelle opportunité pour inspirer, éduquer et connecter. Chaque mot que j'écris, chaque image que je sélectionne, chaque idée que je partage, a le potentiel de toucher une vie, de susciter une passion, de changer une perspective. Je ne suis pas simplement un créateur de contenu, je suis un architecte de possibilités, un catalyseur de changement, un champion de notre mission collective."`,
+    role: "Contenu"
+  },
+  {
+    name: "RAHARINANDRASANA ",
+    firstname: "Willys Sadi Arnel",  
+    image: "./assets/images/teams/Arnel.jpg",
+    description: `La logique et la créativité s'entremêlent dans mon processus de développement, créant une synergie puissante qui alimente mon travail. Je vois chaque bug comme une opportunité d'apprentissage, une chance de perfectionner mes compétences et d'élever mes standards. Dans un monde numérique en perpétuelle évolution, je suis un pilote de l'innovation, naviguant avec assurance vers de nouveaux horizons."`,
+    role: "Développeur"
+  },
+  {
+    name: "RAMAHANDRISOA ",
+    firstname: "Nassi Nassaï Gaël",
+    image: "./assets/images/teams/Gael.png",
+    description: `"Je suis motivé par la possibilité de créer des solutions qui simplifient la vie des gens et améliorent leur quotidien. La technologie est mon moyen d'expression, un langage universel qui transcende les frontières et les barrières. En tant que développeur web, je suis un architecte du changement, bâtissant un avenir où les possibilités sont infinies."`,
+    role: "Développeur"
+  },
+  {
+    name: "RASOANJANAHARY ",
+    firstname: "Christhelen Luicencia",
+    image: "./assets/images/teams/Luicia.jpg",
+    description: `Ce qui me fascine le plus, c'est l'esprit d'équipe qui anime notre club. Grâce au club multimédia, j'ai pu développer des nouvelles compétences. Faire partie de l'équipe de design m'a permis d'explorer ma créativité. Le design donne vie à mes imaginations et j'éprouve une immense satisfaction lorsque j'arrive à créer quelque chose.`,
+    role: "Designer"
+  },
+  {
+    name: "RATIANDRAIBE ",
+    firstname: "Fanampinirina Miharisoa David Fils",
+    image: "./assets/images/teams/David.jpg",
+    description: `"Le développement web est bien plus qu'un simple métier, c'est une passion. Chaque ligne de code est une expression de notre amour pour la technologie, chaque site que nous construisons est une manifestation de notre dévouement à créer des expériences exceptionnelles pour les utilisateurs."`,
+    role: "Développeur"
+  },
+  // {
+  //   name: "RIVONTSOA",
+  //   firstname: "René Brillant",
+  //   image: "./assets/images/teams/Brillant.jpg",
+  //   description: `"En tant que créateurs de contenu web, nous avons le pouvoir de toucher, d'éduquer et d'inspirer des milliers, voire des millions de personnes à travers le monde. Chaque fois que quelqu'un lit nos articles, regarde nos vidéos ou écoute nos podcasts, nous avons l'opportunité de faire une différence dans leur vie. C'est une responsabilité précieuse et une source inépuisable d'inspiration."`,
+  //   role: "Contenu"
+  // },
+  {
+    name: "SEHENOARISOA",
+    firstname: " Adelaïde",
+    image: "./assets/images/teams/Adelaide.jpeg",
+    description: `"Le design de site web est bien plus qu'une simple esthétique, c'est une expérience utilisateur. Chaque élément visuel que nous choisissons, chaque interaction que nous créons, contribue à façonner l'expérience des utilisateurs et à influencer leur perception de la marque. En tant que designers, nous avons le pouvoir de créer des expériences mémorables qui captivent, inspirent et engagent notre public."`,
+    role: "Designer"
+  },
+  {
+    name: "NOMENJANAHARY ",
+    firstname: " Tojonirina Freddy",
+    image: "./assets/images/teams/Freddy2.jpg",
+    description: `"Chaque pixel que nous plaçons sur l'écran est une opportunité de créer une expérience inoubliable pour nos utilisateurs. Notre design transcende les simples lignes de code pour devenir une passerelle vers l'innovation et la beauté. Ensemble, nous sculptons le paysage numérique, offrant un refuge d'harmonie et d'utilité dans un monde de pixels éphémères."`,
+    role: "Designer",
+  }
+];
